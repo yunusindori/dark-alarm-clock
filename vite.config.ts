211f68a81e174f394f2aea76bbb161101a1270ae
yunusindori <yunusindori@gmail.com>
2026-02-21 02:32:11 -0600

    Added explicit server endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 7000, // Change to your desired port number (e.g., 8080, 3000)
+    host: true, // Set to true to listen on all addresses, including LAN and public addresses
+  }
 })
