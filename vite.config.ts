b16be845e2925a72946e370de7bfb68c7c2d603a
yunusindori <yunusindori@gmail.com>
2026-02-21 22:35:15 -0600

    Reverted base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
   server: {
     port: 7000, // Change to your desired port number (e.g., 8080, 3000)
